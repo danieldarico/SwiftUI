c5f13fa1cf99afb40962df0e115cd7950cfc0f86
Daniel Lameu <74214269+danieldarico@users.noreply.github.com>
2023-06-25 16:03:08 -0300

    Update | 25/06/2023 -  ContentView.swift
    New features

--- MyApps | SwiftUI/Shopping/ShoppingKart/ShoppingKart/ContentView.swift
+++ MyApps | SwiftUI/Shopping/ShoppingKart/ShoppingKart/ContentView.swift
@@ -11,52 +11,108 @@ struct ContentView: View {
     
     @State var shoppingItems: [String] = []
     @State var newItem = ""
+    @State private var count = 0
+    @FocusState private var newItemIsFocused: Bool
+    
     
     var body: some View {
         VStack {
             HStack {
-                Image(systemName: "cart")
-                    .foregroundColor(Color.pink)
-                    .bold()
-                Text("Lista de Compras")
+                Text("🛒 Shopping Cart")
                     .font(.title)
-                    .foregroundColor(Color.pink)
+                    .foregroundColor(Color.gray)
                     .bold()
                     .padding()
             }
             
             HStack {
-                TextField("Novo item", text: $newItem)
+                TextField("New item", text: $newItem)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .focused($newItemIsFocused)
                 
-                Button(action: {
+                Button( action: {
+                    
                     addItem()
                 }) {
-                    Text("Adicionar")
-                        .foregroundColor(.white)
+                    Text("Add item")
                         .bold()
-                        .padding(6)
-                        .background(.pink)
-                        .cornerRadius(25)
+                        .padding(7)
+
                 }
             }
+            
             .padding()
+            
+            Text("Itens Added: \(count)")
+                .bold()
+                .font(.callout)
+                .foregroundColor(.blue)
             
             List {
                 ForEach(shoppingItems, id: \.self) { item in
-                    Text(item)
-                        .foregroundColor(.pink)
-                        .bold()
+                    HStack {
+                        Text(item)
+                            .foregroundColor(.blue)
+                            .bold()
+                        Spacer()
+                        Button(action: {
+                            removeLastItem()
+                        }) {
+                            Image(systemName: "cart.badge.minus")
+                                .foregroundColor(.red)
+                        }
+                    }
                 }
+                
+            }
+               
+            if !shoppingItems.isEmpty {
+                Button("Clear all") {
+                    count = 0
+                    clearAll()
+                }
+                .foregroundColor(.white)
+                .bold()
+                .padding(6)
+                .background(Color.black)
+                .cornerRadius(25)
             }
         }
         .padding()
+        .toolbar {
+            ToolbarItemGroup(placement: .keyboard) {
+                Button("Hide Keyboard") {
+                    newItemIsFocused = false
+                }
+                .foregroundColor(.white)
+                .background(Color.red)
+                .cornerRadius(25)
+                .padding(6)
+            }
+        }
     }
     
     func addItem() {
-        shoppingItems.append(newItem)
-        newItem = ""
+        if !newItem.isEmpty {
+            shoppingItems.append(newItem)
+            newItem = ""
+            count += 1
+        }
+
     }
+    
+    func removeLastItem() {
+        if !shoppingItems.isEmpty {
+            shoppingItems.removeLast()
+            count -= 1
+        }
+    }
+    func clearAll() {
+        if !shoppingItems.isEmpty {
+            shoppingItems.removeAll()
+        }
+    }
+
 }
 
 struct ContentView_Previews: PreviewProvider {
